00e64c75d9f72a7040305b37d23fbe0fab1af58e
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-06-01 01:07:59 +0900

    fix: fix fetch code

--- src/pages/FriedEggPage.tsx
+++ src/pages/FriedEggPage.tsx
@@ -10,7 +10,7 @@ const FriedEggPage: React.FC = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const fetchData = await fetch('./public/data.json').then((res) =>
+        const fetchData = await fetch('../../public/data.json').then((res) =>
           res.json(),
         );
 
